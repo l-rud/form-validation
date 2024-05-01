9db169d3cae6d4b91990d6f385bba1fd33317121
Larysa Rud <larpavrud@gmail.com>
2024-04-30 22:37:14 -0400

    Username Validation: the username cannot be blank.

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const registrationForm = document.getElementById("registration");
 registrationForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   registrationForm.submit();
+
+  const userName = registrationForm.elements["username"];
+  if (userName.value === "") {
+    userName.focus();
+    alert("The username cannot be blank");
+    return;
+  }
 });
